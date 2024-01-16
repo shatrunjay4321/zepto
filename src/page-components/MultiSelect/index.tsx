f9fe4fd6f34548463c6f254d90322a0463f49090
shatrunjaysrivastava <shatrunjay.srivastava@cogoport.com>
2024-01-16 21:01:39 +0530

    code cleaning

--- src/page-components/MultiSelect/index.tsx
+++ src/page-components/MultiSelect/index.tsx
@@ -1,53 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from './styles.module.css';
-import data from "configurations/data";
 
-// type MultiSelectProps = {
-//     data: any,
-//     value?: any,
-//     onChange: (value: any) => void,
-//     onSearch: (value: any) => void,
-// }
+interface DataItem {
+    id: number;
+    name: string;
+    email: string;
+  }
 
-const isEmpty = (val) => val.length === 0;
+const isEmpty = (val) => val?.length === 0;
 
-// function MultiSelect({data, value, onChange, onSearch}: MultiSelectProps) {
-function MultiSelect() {
+const MultiSelect: React.FC<{ data: DataItem[] }> = ({ data }) => {
     const [chipClick, setChipClick] = useState({id: 0});
     const [clicked, setClicked] = useState(false);
     const [options, setOptions] = useState(data);
     const [selectedOptions, setSelectedOptions] = useState([]);
+    const [search, setSearch] = useState<string>('');
 
     const handleClick = (item) => {
         const newOptions = options?.filter((val) => val?.id !== item?.id)
         setOptions(newOptions);
         setSelectedOptions([...selectedOptions, item]);
         setChipClick(item);
+        setSearch('');
     }
 
     const handleCross = (item, e) => {
         e.stopPropagation();
         const newSelectedOptions = selectedOptions?.filter((val) => val?.id !== item?.id);
+        const selectedIds = newSelectedOptions?.map((val) => val?.id);
+        const newOptions = data?.filter((val) => !selectedIds?.includes(val?.id));
         setSelectedOptions(newSelectedOptions);
-        setOptions([...options, item]);
+        setOptions(newOptions);
+        setSearch('');
     }
 
     const handleDropDown = (e) => {
         e.stopPropagation(); 
         setClicked((prev) => !prev);
+        setSearch('');
     }
 
     const handleChipClick = (item, e) => {
         e.stopPropagation(); 
         setChipClick(item);
+        setSearch('');
     }
+
+    useEffect(() => {
+        const debounceSearch = () => {
+            const selectedIds = selectedOptions?.map((item) => item?.id);
+            const newOptions = data?.filter((item) => !selectedIds?.includes(item?.id) && item?.name?.toLowerCase().includes(search?.toLowerCase()));
+            setOptions(newOptions);
+        };
+        const timer = setTimeout(debounceSearch, 600); 
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [search]);
 
     return (
     <div className={styles.container}>
         <div className={styles.upper_container} onClick={() => setChipClick({id: 0})}>
             <div className={styles.option_container}>
-                {isEmpty(selectedOptions) ? <div className={styles.placeholder}>Add Items</div> : 
-                (selectedOptions || []).map((item) => {
+                {(selectedOptions || []).map((item) => {
                     return (
                     <div
                         key={item?.id} 
@@ -57,8 +73,15 @@ function MultiSelect() {
                         <div className={styles.chip_left}>{item?.name}</div>
                         <div className={styles.chip_right} onClick={(e) => handleCross(item, e)}>X</div>
                     </div>)
-                })
-                }
+                })}
+                <input 
+                    className={styles.input_field} 
+                    placeholder="Add items" 
+                    type='text' 
+                    value={search} 
+                    onChange={(e) => setSearch(e.target.value)}
+                    onClick={() => setClicked(true)}
+                />
             </div>
             <div onClick={(e) => handleDropDown(e)} 
                 className={`${styles.drop_down_icon} ${clicked ? styles.cliked_dropdown_icon : ''}`}
